27c291faa8e771b9a7e2f58f70134fbfb19b2a2e
pierre_hamy <pierre.hamy@gmail.com>
2021-03-02 23:08:15 +0100

    fix comments and endpoints

--- src/main/java/com/pierre/hamy/micrometer/demo/api/TestController.java
+++ src/main/java/com/pierre/hamy/micrometer/demo/api/TestController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Timed
 public class TestController {
      // cf https://docs.spring.io/spring-boot/docs/2.1.9.RELEASE/reference/html/production-ready-metrics.html#production-ready-metrics-spring-mvc
-    @RequestMapping("/")
+    @RequestMapping("/getResultMetricsAvailableOnActuator")
     @Timed(extraTags = { "region", "us-east-1" })
-    // refer to http://localhost:8080/actuator/metrics/http.server.requests?tag=uri%3A/&tag=region%3Aus-east-1
+    // refer to http://localhost:8080/actuator/metrics/http.server.requests?tag=uri%3A/getResultMetricsAvailableOnActuator&tag=region%3Aus-east-1
+    // !! only available when first request on "http://localhost:8080/getResultMetricsAvailableOnActuator" has been done
     public String getResultMetricsAvailableOnActuator() {
         return "Hello World";
     }
